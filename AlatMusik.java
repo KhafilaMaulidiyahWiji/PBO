ad71b6ce9b1079a5a326afecc3c0ab5095ac03a6
KhafilaMaulidiyahWiji <kfila729@gmail.com>
2025-03-04 09:18:43 +0700

    Update AlatMusik.java

--- AlatMusik.java
+++ AlatMusik.java
@@ -1,10 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package FifthWeeks;
 
 /**
  *
  * @author Khafila Maulidiyah W
  */
-
 // Kelas induk
 class Main {
     void mainkan() {
